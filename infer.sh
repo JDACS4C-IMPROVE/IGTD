4f18bab84faa407ceeccccd2b1b7bdd53997cdc8
zhuyitan <generegu@gmail.com>
2022-11-02 14:45:31 -0500

    Update infer.sh

--- infer.sh
+++ infer.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
+# arg 1 CANDLE_DATA_DIR
+# arg 2 CANDLE_CONFIG
+
 #path=$(realpath "${BASH_SOURCE:-$0}")
 #DIR_PATH=$(dirname $path)
 #Infer_Script="$DIR_PATH/Infer.py"
 
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda-10.0/compat/"
+#export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda-10.0/compat/"
 #echo $LD_LIBRARY_PATH
 
-Infer_Script="/IGTD/Infer.py"
-if [[ "$#" < 1  ]] ; then
+Infer_Script="/usr/local/IGTD/Infer.py"
+if [[ "$#" < 2  ]] ; then
 	    echo "Illegal number of parameters"
-	    echo "CANDLE_DATA_DIR is required"
+	    echo "CANDLE_DATA_DIR CANDLE_CONFIG are required"
 	    exit -1
 fi
 
 CANDLE_DATA_DIR=$1; shift
+CANDLE_CONFIG=$1; shift
+
+export CANDLE_DATA_DIR=$CANDLE_DATA_DIR
 CMD="python3 ${Infer_Script} $@"
 
 
