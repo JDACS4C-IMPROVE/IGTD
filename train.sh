ee9a1fcd7ba49b10c88016157fc4e0aef4d52873
zhuyitan <generegu@gmail.com>
2022-11-02 15:40:40 -0500

    Update train.sh

--- train.sh
+++ train.sh
@@ -14,23 +14,23 @@
 Train_Script="/usr/local/IGTD/Train.py"
 if [[ "$#" < 2  ]] ; then
 	    echo "Illegal number of parameters"
-	    echo "CUDA_VISIBLE_DEVICES CANDLE_DATA_DIR CANDLE_CONFIG are required"
+	    echo "CUDA_VISIBLE_DEVICES CANDLE_DATA_DIR are required"
 	    exit -1
 fi
 
 CUDA_VISIBLE_DEVICES=$1; shift
 CANDLE_DATA_DIR=$1; shift
-CANDLE_CONFIG=$1 ; shift
+# CANDLE_CONFIG=$1 ; shift
 
-export CANDLE_DATA_DIR=$CANDLE_DATA_DIR
-CMD="python3 ${Train_Script} --config_file $CANDLE_CONFIG"
-
+#export CANDLE_DATA_DIR=$CANDLE_DATA_DIR
+#CMD="python3 ${Train_Script} --config_file $CANDLE_CONFIG"
+CMD="python3 ${Train_Script} $@"
 
 
 echo "using container "
 echo "using CUDA_VISIBLE_DEVICES ${CUDA_VISIBLE_DEVICES}"
 echo "using CANDLE_DATA_DIR ${CANDLE_DATA_DIR}"
-echo "using CANDLE_CONFIG ${CANDLE_CONFIG}"
+#echo "using CANDLE_CONFIG ${CANDLE_CONFIG}"
 echo "running command ${CMD}"
 
 CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} CANDLE_DATA_DIR=${CANDLE_DATA_DIR} $CMD
