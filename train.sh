1c24b103fd412c497e2573031244b2a3297d74a2
zhuyitan <generegu@gmail.com>
2022-11-15 17:00:32 -0600

    Add files via upload

--- train.sh
+++ train.sh
@@ -1,36 +1,43 @@
+
 #!/bin/bash
+
+#########################################################################
+### THIS IS A TEMPLATE FILE. SUBSTITUTE #PATH# WITH THE MODEL EXECUTABLE.
+#########################################################################
+
 
 # arg 1 CUDA_VISIBLE_DEVICES
 # arg 2 CANDLE_DATA_DIR
 # arg 3 CANDLE_CONFIG
 
-#path=$(realpath "${BASH_SOURCE:-$0}")
-#DIR_PATH=$(dirname $path)
-#Train_Script="$DIR_PATH/Train.py"
+### Path to your CANDLEized model's main Python script###
+CANDLE_MODEL="/usr/local/IGTD/Train.py"
 
-#export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda-10.0/compat/"
-#echo $LD_LIBRARY_PATH
+if [ $# -lt 2 ] ; then
+	echo "Illegalnumber of paramaters"
+	exit -1
 
-Train_Script="/usr/local/IGTD/Train.py"
-if [[ "$#" < 2  ]] ; then
-	    echo "Illegal number of parameters"
-	    echo "CUDA_VISIBLE_DEVICES CANDLE_DATA_DIR are required"
-	    exit -1
+elif [ $# -eq 3 ] ; then
+  CUDA_VISIBLE_DEVICES=$1
+  CANDLE_DATA_DIR=$2
+  CANDLE_CONFIG=$3
+  CMD="python ${CANDLE_MODEL} --config_file $CANDLE_CONFIG"
+  echo "CMD = $CMD"
+
+else
+  echo "num args is greather than 3 $#"
+  # the candle config is converted to command line parameters
+  CUDA_VISIBLE_DEVICES=$1 ; shift
+  CANDLE_DATA_DIR=/candle_data_dir/$1 ; shift
+  CMD="python ${CANDLE_MODEL} $@"
+  echo "CMD = $CMD"
 fi
 
-CUDA_VISIBLE_DEVICES=$1; shift
-CANDLE_DATA_DIR=$1; shift
-# CANDLE_CONFIG=$1 ; shift
-
-#export CANDLE_DATA_DIR=$CANDLE_DATA_DIR
-#CMD="python3 ${Train_Script} --config_file $CANDLE_CONFIG"
-CMD="python3 ${Train_Script} $@"
-
-
-echo "using container "
+# Display runtime arguments
 echo "using CUDA_VISIBLE_DEVICES ${CUDA_VISIBLE_DEVICES}"
 echo "using CANDLE_DATA_DIR ${CANDLE_DATA_DIR}"
-#echo "using CANDLE_CONFIG ${CANDLE_CONFIG}"
+echo "using CANDLE_CONFIG ${CANDLE_CONFIG}"
 echo "running command ${CMD}"
 
+# Set up environmental variables and execute model
 CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} CANDLE_DATA_DIR=${CANDLE_DATA_DIR} $CMD
