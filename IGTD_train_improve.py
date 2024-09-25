15e3ec6f58c713f6ecd8ba9120aa74a4a8868333
nkoussa <156325369+nkoussa@users.noreply.github.com>
2024-09-25 16:21:26 -0400

    Update IGTD_train_improve.py

--- IGTD_train_improve.py
+++ IGTD_train_improve.py
@@ -162,7 +162,8 @@ def run(params):
         y_pred=valPredResult,
         stage="val",
         y_col_name=params["y_col_name"],
-        output_dir=params["output_dir"])
+        output_dir=params["output_dir"],
+        input_dir=params["input_dir"])
 
     # ------------------------------------------------------
     # [Req] Compute performance scores
